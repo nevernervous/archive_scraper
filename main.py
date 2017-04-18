dd4b415b6620a37f59f31c757f91469a3752e1d2
nevernervous <brndncostoya@gmail.com>
2017-04-19 05:50:00 +0900

    added www.78discography.com scraping function.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import csv
 from myLogger import logger
 import scraper
 
@@ -7,35 +6,73 @@ sites = ['http://www.78discography.com/', 'http://www.45worlds.com/78rpm/', 'htt
 
 def get_certain_link(links):
     for link in links:
-        for site in sites:
-            if link.startswith(site):
-                return
+        for i in range(len(sites)):
+            if link.startswith(sites[i]):
+                return i, link
+    return -1, None
 
-if __name__ == '__main__':
-    dates = []
+
+def main():
     archive_details_urls = []
     google_search_urls = []
-
-    with open('78dates.txt', 'r') as f:
-        reader = csv.reader(f, delimiter='\t')
-        lineNumber = 1
+    with open('78dates.txt', mode='r', encoding='utf-8') as f:
+        row_number = 1
         for row in f:
             data = row.split('\t')
+            row_number += 1
             if len(data) != 3:
-                logger.warning('Input data in {:d} lines is not valid.'.format(lineNumber))
+                logger.warning('Input data in {:d} row is not valid.'.format(row_number))
                 continue
 
-            dates.append(data[0])
+            if data[0]:
+                continue
+
             archive_details_urls.append(data[1])
             google_search_urls.append(data[2])
-            lineNumber += 1
+
+    failed_file = open('failed.csv', 'w')
 
     with open('result.txt', 'w') as f:
-        for i in range(len(dates)):
-            if not dates[i]:
-                links = scraper.google_search(google_search_urls[i])
-                print(google_search_urls[i])
+        for i in range(len(archive_details_urls)):
+            logger.info('Searching for -- {} --: {}'.format(i, archive_details_urls[i]))
+            links = scraper.google_search(google_search_urls[i])
+            if len(links) == 0:
+                logger.warning('No search results.')
+                continue
+
+            site_index, link = get_certain_link(links)
+            if site_index == -1:
+                logger.warning('No matched site.')
+                failed_file.write('{},{}'.format(archive_details_urls[i], google_search_urls[i]))
+                continue
+
+            logger.info('Matched site: {}'.format(link))
+
+            date = ''
+
+            if site_index == 0:
+                continue
+                date = scraper.scrape_78discography(archive_details_urls[i], link)
+            elif site_index == 1:
+                date = scraper.scrape_45worlds(archive_details_urls[i], link)
                 break
+            elif site_index == 2:
+                date = scraper.scrpae_adp(archive_details_urls[i], link)
+                break
+
+            if date == '':
+                logger.warning('Date not found.')
+            else:
+                logger.info('Date: {}'.format(date))
+
+            f.write('{}\t{}\t{}'.format(archive_details_urls[i], date, link))
+
+
+
+
+
+if __name__ == '__main__':
+    main()
 
 
 
--- scraper.py
+++ scraper.py
@@ -1,25 +1,36 @@
-from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import urllib.request
+from urllib.request import urlopen
+from myLogger import logger
+from dateutil.parser import parse
 import re
 import json
+import requests
+
+
+def get_soup(url):
+    url = url.split()
+    url = "+".join(url)
+    r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.101 Safari/537.36'})
+    soup = BeautifulSoup(r.content, "html.parser")
+    return soup
 
 
 def google_search(url):
-    html = url.split()
-    html = "+".join(html)
-    req = urllib.request.Request(html, headers={'User-Agent': 'Mozilla/5.0'})
-
-    soup = BeautifulSoup(urlopen(req).read(), "html.parser")
-
-    # Regex
-    reg = re.compile(".*&sa=")
-
     links = []
-    # Parsing web urls
-    for item in soup.find_all('h3', attrs={'class': 'r'}):
-        line = (reg.match(item.a['href'][7:]).group())
-        links.append(line[:-4])
+    try:
+        url = url.split()
+        url = "+".join(url)
+        r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 '})
+        soup = BeautifulSoup(r.content, "html.parser")
+        # Regex
+        reg = re.compile(".*&sa=")
+
+        # Parsing web urls
+        for item in soup.find_all('h3', attrs={'class': 'r'}):
+            line = (reg.match(item.a['href'][7:]).group())
+            links.append(line[:-4])
+    except Exception as e:
+        logger.error('Google search error: {}'.format(e))
 
     return links
 
@@ -31,8 +42,58 @@ def get_archive_metadata(url):
     catalog_number = metadata['result']['external-identifier'][1]
     catalog_number = catalog_number[catalog_number.rfind(':') + 1:]
     return {
-        'title': metadata['result']['title'],
-        'creator': metadata['result']['creator'][0],
-        'publisher': metadata['result']['publisher'],
-        'catalogNumber': catalog_number
+        'title': metadata['result']['title'].lower(),
+        'creator': metadata['result']['creator'][0].lower(),
+        'publisher': metadata['result']['publisher'].lower(),
+        'catalogNumber': catalog_number.split()[0]
     }
+
+
+def scrape_78discography(archive_url, url):
+    date = ''
+    metadata = get_archive_metadata(archive_url)
+    soup = get_soup(url)
+    publisher = soup.body.h1.center.string.lower()
+
+    if not publisher.startswith(metadata['publisher'].lower()):
+        logger.warning('Publisher is not matched.')
+        return date
+
+    for item in soup.find_all('td', text=metadata['catalogNumber']):
+        td_list = item.parent.select('td')
+
+        # creator = td_list[1].string
+
+        title = td_list[2].string
+
+        if title.lower() == metadata['title']:
+            date = date_parsing(td_list[6].string)
+            break
+
+    return date
+
+
+def scrape_45worlds(archive_url, url):
+    date = ''
+
+    metadata = get_archive_metadata(archive_url)
+    soup = get_soup(url)
+    # publisher = soup.body.h1.center.string.lower()
+    return date
+
+
+def scrpae_adp(archive_url, url):
+    date = ''
+    return date
+
+
+def date_parsing(date_string):
+    if date_string == '' or date_string == '-':
+        return ''
+    year = str(parse(date_string).year)
+
+    if not year:
+        return ''
+
+    return '19' + year[2:]
+
